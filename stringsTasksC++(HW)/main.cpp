58269bee2fa676ad42fa789f8cc18683074a47f1
antonkurin <antonkurin1@gmail.com>
2021-10-29 15:48:47 +0300

    Initial Commit

---
+++ stringsTasksC++(HW)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  stringsTasksC++(HW)
+//
+//  Created by Антон Курин on 29.10.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
